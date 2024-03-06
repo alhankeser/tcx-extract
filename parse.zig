81f9bc9d2349fe51fefaa72aa3dcb2f0dfd0b3ef
Alhan Keser <alhankeser@gmail.com>
2024-03-06 07:39:58 +0100

    Fix for cases where there are null data point

--- parse.zig
+++ parse.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
+const stdout = std.io.getStdOut().writer();
+const spineTagName = "<Trackpoint>";
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -16,16 +18,43 @@ pub fn main() !void {
     defer allocator.free(readBuf);
 
     const targetTagText: []u8 = args[2];
-    const targetTagName = try std.fmt.allocPrint(allocator, "<{s}>", .{targetTagText});
-    defer allocator.free(targetTagName);
+    const targetTagStart = try std.fmt.allocPrint(allocator, "<{s}>", .{targetTagText});
+    defer allocator.free(targetTagStart);
+    const targetTagEnd = try std.fmt.allocPrint(allocator, "</{s}>", .{targetTagText});
+    defer allocator.free(targetTagEnd);
 
-    var tags = std.mem.split(u8, readBuf, targetTagName);
-    const stdout = std.io.getStdOut().writer();
-    _ = tags.next();
-    while (tags.next()) |tagContent| {
-        var tagSplit = std.mem.split(u8, tagContent, "</");
-        while (tagSplit.next()) |tag| {
-            _ = try stdout.write(try std.fmt.allocPrint(allocator, "{s},", .{tag}));
+    var points = std.mem.split(u8, readBuf, spineTagName);
+    
+
+    // This looks worse than it is: 
+    // Say we have this: 
+    // <Trackpoint>
+            // <MyTargetValue>2024-03-03T06:01:29.000Z</MyTargetValue>
+            // <Altitude>123</Altitude>
+    // </Trackpoint>
+    // We split the whole thing by <Trackpoint>
+    // We want a row per Trackpoint, otherwise, we could have incomplete data 
+    // If, say, MyTargetValue is null on some Trackpoints, we want to capture that null
+    // We split the first substring by </MyTargetValue>
+    // Then split the first resulting substring by <MyTargetValue>
+    // And return the first substring, which should be the value in the tag
+    // If I knew how to index the result of a mem.split, I could avoid using while()
+    var i: i8 = 0;
+    _ = points.next();
+    while (points.next()) |point| {
+        var tagBeforeAfters = std.mem.split(u8, point, targetTagEnd);
+        while (tagBeforeAfters.next()) |tagBeforeAfter| {
+            var tagAfter = std.mem.split(u8, tagBeforeAfter, targetTagStart);
+            _ = tagAfter.next();
+            while (tagAfter.next()) |tag| {                
+                _ = try stdout.write(try std.fmt.allocPrint(allocator, "{s}", .{tag}));
+                break;
+            }
+            _ = try stdout.write("\n");
+            break;
+        }
+        i += 1;
+        if (i > 10) {
             break;
         }
     }
